411b7f10584227d66efe5db87144d1bc57857048
Данил Юдин <dayudin78@gmail.com>
2025-05-14 16:31:40 +0300

    add helpers module

---
+++ helpers.py
@@ -0,0 +1,18 @@
+def get_comment(event: dict, *args: str) -> str:
+    current = event
+    for key in args:
+        if not isinstance(current, dict):
+            return ""
+        current = current.get(key)
+    return current if isinstance(current, str) else ""
+
+
+def get_repo_name(event: dict) -> str:
+    repo_name: str = event.get("repo", {}).get("name", "unknown repo")
+    return repo_name
+
+
+def print_optional_message(comment: str) -> None:
+    print(
+        f"-> {comment or 'You can display the message by adding --show-messages'}"
+    )
