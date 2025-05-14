c3f6cdd52c7c7b42b00d2f22e324728bb22a02c6
Данил Юдин <dayudin78@gmail.com>
2025-05-14 14:53:22 +0300

    modify main

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import requests
 import json
 from utils import handle_activity
 
-username: str = "orbeckst"
-url: str = f"https://api.github.com/users/{username}/events/public"
 
-response = requests.get(url)
+if __name__ == "__main__":
+    username: str = "orbeckst"
+    url: str = f"https://api.github.com/users/{username}/events/public"
 
-if response.status_code == 200:
-    events: list[dict] = response.json()
-    handle_activity(events=events)
+    response = requests.get(url)
 
-with open("response.json", "w") as file:
-    json.dump(events, file, indent=4)
+    if response.status_code == 200:
+        events: list[dict] = response.json()
+        handle_activity(events=events, show_messages=True)
+
+    with open("response.json", "w") as file:
+        json.dump(events, file, indent=4)
